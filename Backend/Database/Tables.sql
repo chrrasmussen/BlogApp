8c173a4b0a2035807c788aec25c7441c9c30e93e
Christian Rasmussen <christian.rasmussen@me.com>
2012-04-21 20:14:12 +0200

    Updated SQL-tables.

--- Backend/Database/Tables.sql
+++ Backend/Database/Tables.sql
@@ -1,19 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9.2
+-- version 3.3.7deb7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 12, 2012 at 11:17 PM
--- Server version: 5.5.9
--- PHP Version: 5.3.6
+-- Generation Time: Apr 21, 2012 at 08:00 PM
+-- Server version: 5.1.61
+-- PHP Version: 5.3.3-7+squeeze8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `blogapp`
 --
-CREATE DATABASE `blogapp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `blogapp`;
 
 -- --------------------------------------------------------
 
@@ -21,16 +25,22 @@ USE `blogapp`;
 -- Table structure for table `comments`
 --
 
-CREATE TABLE `comments` (
+CREATE TABLE IF NOT EXISTS `comments` (
   `commentId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `postId` int(10) unsigned NOT NULL,
   `userId` int(10) unsigned DEFAULT NULL,
   `name` varchar(40) NOT NULL,
   `body` text NOT NULL,
+  `createdAt` datetime NOT NULL,
   PRIMARY KEY (`commentId`),
   KEY `userId` (`userId`),
   KEY `postId` (`postId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `comments`
+--
+
 
 -- --------------------------------------------------------
 
@@ -38,15 +48,24 @@ CREATE TABLE `comments` (
 -- Table structure for table `posts`
 --
 
-CREATE TABLE `posts` (
+CREATE TABLE IF NOT EXISTS `posts` (
   `postId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `userId` int(10) unsigned NOT NULL,
   `title` varchar(200) NOT NULL,
   `body` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `createdAt` datetime NOT NULL,
+  `modifiedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`postId`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`postId`, `userId`, `title`, `body`, `createdAt`, `modifiedAt`) VALUES
+(1, 1, 'First Blog Post!', '<p>This is my first official blog post.</p>', '2012-02-26 16:11:11', '2012-04-21 05:17:44'),
+(2, 1, 'This is Another Blog Post', '<p>This entry is a test of the different font styles.</p><p>I can use <b>bold</b>, <i>italic</i> and <u>underlined</u> text as I desire. I can also create new headings:</p>\n<h3>Heading</h3>\n<p>More text...</p>', '2012-04-01 23:05:35', '2012-04-21 19:11:21');
 
 -- --------------------------------------------------------
 
@@ -54,12 +73,23 @@ CREATE TABLE `posts` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `userId` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(40) NOT NULL,
+  `fullName` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` char(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `authorizationLevel` smallint(6) NOT NULL,
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `username` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `fullName`, `email`, `password`, `authorizationLevel`) VALUES
+(1, 'Christian Rasmussen', 'c@r.io', 'christian', 10),
+(2, 'Demo User', 'demo@demo.com', 'demo', 10);
 
 --
 -- Constraints for dumped tables
@@ -69,8 +99,8 @@ CREATE TABLE `users` (
 -- Constraints for table `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE SET NULL ON UPDATE NO ACTION,
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE SET NULL ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `posts`
