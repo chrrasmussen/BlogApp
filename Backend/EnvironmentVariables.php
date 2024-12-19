726af4eda137a63126a38ec0b13b375b6630d0f2
Christian Rasmussen <christian.rasmussen@me.com>
2024-12-19 20:13:13 +0100

    Update environment variables

--- Backend/EnvironmentVariables.php
+++ Backend/EnvironmentVariables.php
@@ -6,20 +6,10 @@
  * @author Christian Rasmussen <christian.rasmussen@me.com>
  */
 
-// Prod
-$_ENV['mysql']['host'] = 'blogapp_db_1';
+$_ENV['mysql']['host'] = 'db';
 $_ENV['mysql']['port'] = 3306;
 $_ENV['mysql']['database'] = 'blogapp';
 $_ENV['mysql']['username'] = 'root';
 $_ENV['mysql']['password'] = 'root';
 
-$_ENV['app']['baseURL'] = 'http://christian.rasmussen.io/cv/blogapp/preview';
-
-// Dev
-// $_ENV['mysql']['host'] = 'db';
-// $_ENV['mysql']['port'] = 3307;
-// $_ENV['mysql']['database'] = 'blogapp';
-// $_ENV['mysql']['username'] = 'root';
-// $_ENV['mysql']['password'] = 'root';
-
-// $_ENV['app']['baseURL'] = 'http://' . $_SERVER['SERVER_NAME'] . ':8888/BlogApp/public_html';
+$_ENV['app']['baseURL'] = ''; // Do not add a trailing slash. Empty string means that base URL is `/`.
