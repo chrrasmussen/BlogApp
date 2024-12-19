87ed132cf2408598dac0d5d5c04504eb53b50b56
Christian Rasmussen <christian.rasmussen@me.com>
2024-12-19 20:14:52 +0100

    Disable error reporting

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,5 @@
 <?php
 
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 'On');
-
-
 header('Content-Type: text/html; charset=utf-8');
 
 require_once(__DIR__ . '/../Backend/App.php');
